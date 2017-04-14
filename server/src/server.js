ae5547b3bee6f47a389555cf0f4c71460d340a69
Sam McGuire <sam.holmes.mcg@gmail.com>
2017-04-14 19:20:47 -0400

    static

--- server/src/server.js
+++ server/src/server.js
@@ -11,25 +11,28 @@ var reverseString = utils.reverseString;
 var bodyParser = require('body-parser');
 app.use(bodyParser.text());
 
+//use express to serve up static files from client/build
+app.use(express.static('../client/build'));
+
 // Defines what happens when it receives the `GET /` request
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+// app.get('/', function (req, res) {
+//   res.send('Hello World!');
+// });
 
 // Starts the server on port 3000!
 app.listen(3000, function (){
   console.log('Example app listening on port 3000!');
 });
 
-// Handle POST /reverse [data]
-app.post('/reverse', function (req, res){
-  // If the request came with text, then the text() middleware handled it
-  // and made `req.body` a string.
-  // Check that req.body is a string.
-  if (typeof(req.body) === 'string') {
-    var reversed = reverseString(req.body);
-    res.send(reversed)
-  } else {
-    res.status(400).end();
-  }
-});
+// // Handle POST /reverse [data]
+// app.post('/reverse/', function (req, res){
+//   // If the request came with text, then the text() middleware handled it
+//   // and made `req.body` a string.
+//   // Check that req.body is a string.
+//   if (typeof(req.body) === 'string') {
+//     var reversed = reverseString(req.body);
+//     res.send(reversed)
+//   } else {
+//     res.status(400).end();
+//   }
+// });
