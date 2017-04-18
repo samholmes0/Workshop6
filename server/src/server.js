3267da9853c2bf9c1deaa6cccf7a1ad4614587c3
Sam McGuire <sam.holmes.mcg@gmail.com>
2017-04-18 18:58:32 -0400

    validation

--- server/src/server.js
+++ server/src/server.js
@@ -7,20 +7,28 @@ var app = express();
 var utils = require('./util');
 //import middleware to handle requests that identify as text
 var bodyParser = require('body-parser');
+// Support receiving text in HTTP request bodies
 app.use(bodyParser.text());
+// Support receiving JSON in HTTP request bodies
+app.use(bodyParser.json());
 
 //use express to serve up static files from client/build
 app.use(express.static('../client/build'));
+
+//schemas
+var StatusUpdateSchema = require('./schemas/statusupdate.json');
+
+//schema validation methods
+var database = require('./database');
+var validate = require('express-jsonschema').validate;
+var writeDocument = database.writeDocument;
+var addDocument = database.addDocument;
 
 // Defines what happens when it receives the `GET /` request
 // app.get('/', function (req, res) {
 //   res.send('Hello World!');
 // });
 
-// Starts the server on port 3000!
-app.listen(3000, function (){
-  console.log('Example app listening on port 3000!');
-});
 
 /**
  * Get the feed data for a particular user.
@@ -117,3 +125,86 @@ function getFeedData(user) {
   // Return FeedData with resolved references.
   return feedData;
 }
+
+/**
+ * Adds a new status update to the database.
+ */
+function postStatusUpdate(user, location, contents) {
+  // If we were implementing this for real on an actual server, we would check
+  // that the user ID is correct & matches the authenticated user. But since
+  // we're mocking it, we can be less strict.
+
+  // Get the current UNIX time.
+  var time = new Date().getTime();
+  // The new status update. The database will assign the ID for us.
+  var newStatusUpdate = {
+    "likeCounter": [],
+    "type": "statusUpdate",
+    "contents": {
+      "author": user,
+      "postDate": time,
+      "location": location,
+      "contents": contents,
+      "likeCounter": []
+    },
+    // List of comments on the post
+    "comments": []
+  };
+
+  // Add the status update to the database.
+  // Returns the status update w/ an ID assigned.
+  newStatusUpdate = addDocument('feedItems', newStatusUpdate);
+
+  // Add the status update reference to the front of the current user's feed.
+  var userData = readDocument('users', user);
+  var feedData = readDocument('feeds', userData.feed);
+  feedData.contents.unshift(newStatusUpdate._id);
+
+  // Update the feed object.
+  writeDocument('feeds', feedData);
+
+  // Return the newly-posted object.
+  return newStatusUpdate;
+}
+
+// `POST /feeditem { userId: user, location: location, contents: contents  }`
+app.post('/feeditem', 
+         validate({ body: StatusUpdateSchema }), function(req, res) {
+  // If this function runs, `req.body` passed JSON validation!
+  var body = req.body;
+  var fromUser = getUserIdFromToken(req.get('Authorization'));
+
+  // Check if requester is authorized to post this status update.
+  // (The requester must be the author of the update.)
+  if (fromUser === body.userId) {
+    var newUpdate = postStatusUpdate(body.userId, body.location, 
+                                 body.contents);
+    // When POST creates a new resource, we should tell the client about it
+    // in the 'Location' header and use status code 201.
+    res.status(201);
+    res.set('Location', '/feeditem/' + newUpdate._id);
+     // Send the update!
+    res.send(newUpdate);
+  } else {
+    // 401: Unauthorized.
+    res.status(401).end();
+  }
+});
+
+// Starts the server on port 3000!
+app.listen(3000, function (){
+  console.log('Example app listening on port 3000!');
+});
+
+/**
+ * Translate JSON Schema Validation failures into error 400s.
+ */
+app.use(function(err, req, res, next) {
+  if (err.name === 'JsonSchemaValidation') {
+    // Set a bad request http response status
+    res.status(400).end();
+  } else {
+    // It's some other sort of error; pass it to next error middleware handler
+    next(err);
+  }
+});
